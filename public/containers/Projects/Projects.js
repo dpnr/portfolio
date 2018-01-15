cfafa236dadad9553d67cbb7d49071a6d4851c48
u1068846 <u1068846@utah.edu>
2018-01-15 10:42:36 -0700

    small changes in the status of the projects

--- public/containers/Projects/Projects.js
+++ public/containers/Projects/Projects.js
@@ -19,25 +19,25 @@ render(){
             <Heading>Projects </Heading>
             
             <Underline className='col-md-12 underline'>
-            <Projectname>Finally Machine learning! <em style={{"fontSize":"1.0em", "fontWeight":"500"}}>(ongoing)</em></Projectname>
+            <Projectname>Finally Machine learning! <em style={{"fontSize":"1.0em", "fontWeight":"500"}}></em></Projectname>
             </Underline>
             
                 <Matter>The main goal is to build a machine learning (ML) classifier to efficiently perform the task of 
                     classifying the data of Twitter users. The classifier is supposed to predict if the user is a content 
-                    polluter or not. Building in Python and no ML packages are used.
+                    polluter or not. Built in Python and no ML packages are used.
                 </Matter>
           
             <br/>
             <br/>
 
             <Underline className='col-md-12 underline'>
-            <Projectname>Event Extractions <em style={{"fontSize":"1.0em", "fontWeight":"500"}}>(ongoing)</em></Projectname>
+            <Projectname>Event Extractions <em style={{"fontSize":"1.0em", "fontWeight":"500"}}></em></Projectname>
             </Underline>
             
-                <Matter>Building an information extraction(IE) system for the news articles as part of the Natural Language 
+                <Matter>Built an information extraction(IE) system for the news articles as part of the Natural Language 
                     Processing (NLP) course work. Making use of the different NLP techniques 
                     to extract several types of information. Implementing it completely using Python and no libraries 
-                    which does any means of event extractions are used.
+                    which does any means of event extractions are used. Awarded the best poster for the final project presentation.
                 </Matter>
           
             <br/>
@@ -52,7 +52,7 @@ render(){
                     the Visualization tools, some as a part of research at the university and some are at the 
                     industry level. Unfortunately, I cannot share details on those projects but will mention the 
                     technologies I have been using. I worked on <em>MEAN</em> stack and recently started working 
-                    with <em>React</em>. Used <em>React + Express</em> for building this website as well.
+                    with <em>ReactJS</em>. Used <em>ReactJS + ExpressJS</em> for building this website as well.
                 </Matter>
           
             <br/>
@@ -60,7 +60,7 @@ render(){
             <Underline className='col-md-12 underline'>   
             <Projectname>My first ever interactive webpage</Projectname>
             </Underline>
-                <Matter>Have worked on this as a part of Data Visualization course I have taken
+                <Matter>Worked on this as a part of Data Visualization course I have taken
                     and that is when I started to love building the analytic tools and also 
                     web development in general. We used HTML, CSS, JavaScript and d3.js for building the page.
                     <br /><br /><Customlink  href="https://u1068846.github.io/VisProject/gradSchoolFinder.html">
@@ -72,7 +72,7 @@ render(){
             <Underline className='col-md-12 underline'> 
             <Projectname>Deep into Databases</Projectname>
             </Underline>
-                <Matter>Have worked on this as a part of Advanced Database Systems course and 
+                <Matter>Worked on this as a part of Advanced Database Systems course and 
                     this project has helped me get to the bits and pieces of building a database system. This project is done completely in Java.
                 <br/>
                 </Matter>
